7ef57dbb10e058cdbd56e6a246ffb0c5f14167b2
cleamm <ehdgus275@naver.com>
2024-06-29 17:53:42 +0900

    헤더 부분 네비게이션으로 교체

--- chatPage/urls.py
+++ chatPage/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='chatPage'),
     path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
-    path('', views.index),
 ]
