e5c2bf7be3f4aedabadeb938734f78eb14bcbea9
cleamm <ehdgus275@naver.com>
2024-06-29 14:56:39 +0900

    update

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'common.apps.CommonConfig',
     'chatPage.apps.ChatpageConfig',
     'django.contrib.admin',
     'django.contrib.auth',
