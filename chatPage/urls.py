ef66ad0ba13bb73d09cd5663ef4e78974cffdda8
cleamm <ehdgus275@naver.com>
2024-06-29 14:55:21 +0900

    로그인 및 회원가입 페이지 연결

--- chatPage/urls.py
+++ chatPage/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
     path('', views.index),
-    path('chat', views.index),
 ]
