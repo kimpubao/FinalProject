5d8fe034d09f6995273ccc152dcc4489835f5bb0
cleamm <ehdgus275@naver.com>
2024-07-02 15:54:51 +0900

    회원가입 기능 추가

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    path('chatPage/', include('chatPage.urls')),
+    path('admin/', admin.site.urls),
+    # path('chatPage/', include('chatPage.urls')),
     path('', include('chatPage.urls')),
 ]
