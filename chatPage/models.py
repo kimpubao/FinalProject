77c1cd9a5456cee2a0eb382413c56fc9c579588c
cleamm <ehdgus275@naver.com>
2024-07-26 17:14:24 +0900

    signup page modify

--- chatPage/models.py
+++ chatPage/models.py
@@ -1,12 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+from config import settings
 
 class Question(models.Model):
-    author=models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    author=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
     content = models.TextField()
     create_date = models.DateTimeField()
 
 class Answer(models.Model):
-    question = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    question = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
     content = models.TextField()
     create_date = models.DateTimeField()
+
+class User(AbstractUser):
+    gender = models.CharField(max_length=1, default="", null=True, blank=True)
+    genre = models.CharField(max_length=50, null=True, blank=True)
+    age_group = models.IntegerField(null=True, blank=True)
